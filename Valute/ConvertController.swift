5c6d4e0ef2a475707230d65a3920d3ca3fb6c3c9
Mladen <mladen1markovic.93@gmail.com>
2017-09-07 17:11:54 +0200

    Initial commit

---
+++ Valute/ConvertController.swift
@@ -0,0 +1,38 @@
+//
+//  ConvertController.swift
+//  Valute
+//
+//  Created by Macintosh on 9/6/17.
+//  Copyright © 2017 Macintosh. All rights reserved.
+//
+
+import UIKit
+
+class ConvertController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        title = "Currency Converter"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
